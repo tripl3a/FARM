c5f272570c0ed9ee356af940f2fd04d29deec2f8
tripl3a <triple_a@mailbox.org>
2019-07-27 15:32:57 +0200

    run doc_classification example

--- hate_detector/run_experiments.py
+++ hate_detector/run_experiments.py
@@ -1,3 +1,6 @@
-from farm.experiment import run_experiment, load_experiments
-experiments = load_experiments("experiments/text_classification/germEval18Coarse_config.json")
-run_experiment(experiments[0])
+# from farm.experiment import run_experiment, load_experiments
+# experiments = load_experiments("experiments/text_classification/germEval18Coarse_config.json")
+# run_experiment(experiments[0])
+
+# import another script to actually execute it
+from examples import doc_classification
