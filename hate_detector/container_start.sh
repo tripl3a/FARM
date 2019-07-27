cbd1ca6593553168f7c59dae5bc6d193a2df7f41
tripl3a <triple_a@mailbox.org>
2019-07-27 14:50:36 +0200

    fixed typo in container start script

--- hate_detector/container_start.sh
+++ hate_detector/container_start.sh
@@ -3,7 +3,4 @@
 # install FARM itself (requirements should already be satisfied from Docker image but will be extended as needed)
 git clone -b nohate https://github.com/tripl3a/FARM.git && cd FARM && pip install -r requirements.txt && pip install .
 
-echo "Current directory: " $(pwd)
-
-cd FARM &&
-python hate_dector/run_experiments.py
+python hate_detector/run_experiments.py
