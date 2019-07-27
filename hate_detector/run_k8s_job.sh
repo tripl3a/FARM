bff58a7ff0f08e17cee10de7067103a7e9330746
tripl3a <triple_a@mailbox.org>
2019-07-27 10:41:15 +0200

    clone nohate branch

--- hate_detector/run_k8s_job.sh
+++ hate_detector/run_k8s_job.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # install FARM itself (requirements should already be satisfied from Docker image but will be extended as needed)
-git clone https://github.com/tripl3a/FARM.git && cd FARM && pip install -r requirements.txt && pip install .
+git clone -b nohate https://github.com/tripl3a/FARM.git && cd FARM && pip install -r requirements.txt && pip install .
 
 python hate_dector/run_experiments.py
