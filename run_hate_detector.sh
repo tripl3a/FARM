6514893075695cdace18256c76fc5305217a85a9
tripl3a <triple_a@mailbox.org>
2019-07-28 16:47:29 +0200

    prettified run script

--- run_hate_detector.sh
+++ run_hate_detector.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-#export RUN_PY_SCRIPT=hate_detector/nohate_classifier.py
-#export RUN_PY_SCRIPT=hate_detector/run_experiments.py
+if [ "$HOSTNAME" = "tripl3a-t440s" ]; then
+  export RUN_PY_SCRIPT=hate_detector/nohate_classifier.py
+  #export RUN_PY_SCRIPT=hate_detector/run_experiments.py
+  export CACHE_DIR=/tlhd/cache
+  export DATA_DIR=/tlhd/data/modeling/FU_data_full
+  export OUTPUT_DIR=/tlhd/models/nohate01
+fi
 
-#if [ "$HOSTNAME" = "tripl3a-t440s" ]; then
-#  export CACHE_DIR=/tlhd/cache
-#  export DATA_DIR=/tlhd/data/modeling/FU_data_full
-#  export OUTPUT_DIR=/tlhd/models/nohate01
-#else
-#  export CACHE_DIR=/home/aallhorn/cache
-#  export DATA_DIR =/home/aallhorn/data/FU_data_full
-#  export OUTPUT_DIR=/home/aallhorn/output/nohate01
-#fi
+if [ "$RUN_PY_SCRIPT" = hate_detector/nohate_classifier.py ]; then
+  echo "*********************************"
+  echo "Running Python script: " $RUN_PY_SCRIPT
+  echo "with args..." $RUN_PY_SCRIPT
+  echo "--> CACHE_DIR=" $CACHE_DIR
+  echo "--> DATA_DIR=" $DATA_DIR
+  echo "--> OUTPUT_DIR=" $OUTPUT_DIR
+  echo "*********************************"
+  python $RUN_PY_SCRIPT  \
+    --cache_dir $CACHE_DIR \
+    --data_dir $DATA_DIR \
+    --output_dir $OUTPUT_DIR
+else
+  echo "Running Python script: " $RUN_PY_SCRIPT
+  python $RUN_PY_SCRIPT
+fi
 
-echo "Running Python script: " $RUN_PY_SCRIPT
-python $RUN_PY_SCRIPT  \
-  --cache_dir $CACHE_DIR \
-  --data_dir $DATA_DIR \
-  --output_dir $OUTPUT_DIR
+
+
