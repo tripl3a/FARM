19ba287886f9edd645e360eadcb1c0c55eb16eb3
tripl3a <triple_a@mailbox.org>
2019-08-02 14:38:28 +0200

    Revert "possibility to supply true_labels in inference for qualitative analysis (TextClassification only)"
    This reverts commit 5d46deb2

--- hate_detector/nohate_clf_infer.py
+++ hate_detector/nohate_clf_infer.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+#%%
 from farm.infer import Inferencer
 import pandas as pd
 import os
@@ -7,34 +7,38 @@ import os
 DATA_DIR = "/tlhd/data/modeling/FU_data_subsample"
 OUTPUT_DIR = "/tlhd/models/nohate01"
 
-# load test data
+# Load saved model to make predictions
+model = Inferencer.load(OUTPUT_DIR)
+
+# store texts with predictions for qualitative analysis
+
 test_file = os.path.join(DATA_DIR, "coarse_test.tsv")
 df_test = pd.read_csv(filepath_or_buffer=test_file, delimiter="\t")
 
 # build list of dicts for FARM
 texts = []
-for index, row in df_test.iterrows():
-    texts.append({"text": row["text"],
-                  "true_label": row["label"]})
+for text in df_test["text"].values:
+    texts.append({"text": text,
+                  "y_true": 1})
 
-# Load saved model and make predictions
-model = Inferencer.load(OUTPUT_DIR)
 result = model.run_inference(dicts=texts)
 
-y_true, y_pred, probs, contexts = [], [], [], []
+y_pred, probs, tasks = [], [], []
+i = 0
 for batch in result:
     for p in batch["predictions"]:
-        y_true.append(p["true_label"])
+        if df_test.iloc[i]["text"] != p["context"]:
+            raise ValueError("Order of input data and inference results does not match!")
+        i += 1
         y_pred.append(p["label"])
         probs.append(p["probability"])
-        contexts.append(p["context"])
+        tasks.append(batch["task"])
 
+#%%
 df_result = pd.DataFrame({
-    "y_true": y_true,
+    "y_true": df_test["label"],
     "y_pred": y_pred,
     "probability": probs,
-    "text": contexts
+    "text": df_test["text"]
 })
 print(df_result)
-
-
