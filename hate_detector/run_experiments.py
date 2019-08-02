5527c4803203684fbd75827d499aa520b023149d
tripl3a <triple_a@mailbox.org>
2019-08-02 11:08:12 +0200

    added logging

--- hate_detector/run_experiments.py
+++ hate_detector/run_experiments.py
@@ -1,3 +1,28 @@
+import logging
 from farm.experiment import run_experiment, load_experiments
-experiments = load_experiments("experiments/text_classification/germEval18Coarse_config.json")
-run_experiment(experiments[0])
+from farm.utils import MLFlowLogger
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+
+    experiments = load_experiments("experiments/text_classification/germEval18Coarse_config.json")
+
+    for args in experiments:
+        logger.info(
+            "\n***********************************************"
+            f"\n************* Experiment: {args.name} ************"
+            "\n************************************************"
+        )
+        ml_logger = MLFlowLogger(tracking_uri=args.mlflow_url)
+        ml_logger.init_experiment(
+            experiment_name=args.mlflow_experiment,
+            run_name=args.mlflow_run_name,
+            nested=args.mlflow_nested,
+        )
+        run_experiment(args)
+
+
+if __name__ == "__main__":
+    main()
