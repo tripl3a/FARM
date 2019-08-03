d509062f2df6e5148e29e3a3922aed83b2114622
tripl3a <triple_a@mailbox.org>
2019-08-03 16:11:37 +0200

    run script

--- run_hate_detector.sh
+++ run_hate_detector.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ "$HOSTNAME" = "tripl3a-t440s" ]; then
-  export RUN_PY_SCRIPT=hate_detector/nohate_clf_train.py
-  #export RUN_PY_SCRIPT=hate_detector/run_nohate_experiments.py
+  #export RUN_PY_SCRIPT=hate_detector/nohate_clf_train.py
+  export RUN_PY_SCRIPT=run_nohate_experiments.py
   export CACHE_DIR=/tlhd/cache
-  export DATA_DIR=/tlhd/data/modeling/FU_data_full
+  export DATA_DIR=/tlhd/data/modeling/FU_data_subsample
   export OUTPUT_DIR=/tlhd/models/nohate01
 fi
 
