1fab0730bd188da1e2e6c03d5957e5bed7882983
tripl3a <triple_a@mailbox.org>
2019-10-03 15:42:44 +0200

    log lr whilst training

--- farm/train.py
+++ farm/train.py
@@ -191,7 +191,7 @@ class Trainer:
                 )
                 for param_group in self.optimizer.param_groups:
                     param_group["lr"] = lr_this_step
-                # MlLogger.write_metrics({"learning_rate": lr_this_step}, step=self.global_step)
+                MlLogger.write_metrics({"learning_rate": lr_this_step}, step=self.global_step)
             self.optimizer.step()
             self.optimizer.zero_grad()
 
