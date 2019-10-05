29e55d0e8f1273001ebe1ba36cefae303142107e
tripl3a <triple_a@mailbox.org>
2019-10-05 12:52:30 +0200

    simplified run shell scripts

--- run_hate_detector.sh
+++ run_hate_detector.sh
@@ -8,58 +8,17 @@ if [ "$HOSTNAME" = "tripl3a-t440s" ]; then
   #export OUTPUT_DIR=/tlhd/models/nohate01
 fi
 
-case "$RUN_PY_SCRIPT" in
-  hate_detector/nohate_clf_train.py)
-    echo "*********************************"
-    echo "Running Python script: " $RUN_PY_SCRIPT
-    echo "with args..." $RUN_PY_SCRIPT
-    echo "--> CACHE_DIR=" $CACHE_DIR
-    echo "--> DATA_DIR=" $DATA_DIR
-    echo "--> OUTPUT_DIR=" $OUTPUT_DIR
-    echo "*********************************"
-    python $RUN_PY_SCRIPT  \
-      --cache_dir $CACHE_DIR \
-      --data_dir $DATA_DIR \
-      --output_dir $OUTPUT_DIR
-    ;;
-  hate_detector/nohate_lm_finetuning.py)
-    if [ "$DISTRIBUTED" = "true" ]; then
-      echo "Launching distributed training..."
-      python -m torch.distributed.launch --nproc_per_node=$NPROC_PER_NODE $RUN_PY_SCRIPT \
-      --bert_model=$BERT_MODEL \
-      --data_dir=$DATA_DIR \
-      --output_dir=$OUTPUT_DIR \
-      --num_train_epochs=$NUM_TRAIN_EPOCHS \
-      --learning_rate=$LEARNING_RATE \
-      --max_seq_length=$MAX_SEQ_LENGTH \
-      --train_batch_size=$TRAIN_BATCH_SIZE \
-      --eval_every=$EVAL_EVERY \
-      --warmup_proportion=$WARMUP_PROPORTION \
-      --mlflow_run_name=$MLFLOW_RUN_NAME \
-      --embeds_dropout_prob=$EMBEDS_DROPOUT_PROB \
-      $ADDITIONAL_ARGS
-    else
-      python $RUN_PY_SCRIPT \
-      --bert_model=$BERT_MODEL \
-      --data_dir=$DATA_DIR \
-      --output_dir=$OUTPUT_DIR \
-      --num_train_epochs=$NUM_TRAIN_EPOCHS \
-      --learning_rate=$LEARNING_RATE \
-      --max_seq_length=$MAX_SEQ_LENGTH \
-      --train_batch_size=$TRAIN_BATCH_SIZE \
-      --eval_every=$EVAL_EVERY \
-      --warmup_proportion=$WARMUP_PROPORTION \
-      --mlflow_run_name=$MLFLOW_RUN_NAME \
-      --embeds_dropout_prob=$EMBEDS_DROPOUT_PROB \
-      $ADDITIONAL_ARGS
-    fi
-      ;;
-  *)
-    echo "Running Python script: " $RUN_PY_SCRIPT
-    python $RUN_PY_SCRIPT \
-      $PARAMETERS
-    ;;
-esac
+echo "Running Python script: " $RUN_PY_SCRIPT
+echo "with parameters: " $PARAMETERS
+
+if [ "$DISTRIBUTED" = "true" ]; then
+  echo "Launching distributed training..."
+  python -m torch.distributed.launch --nproc_per_node=$NPROC_PER_NODE $RUN_PY_SCRIPT \
+  $PARAMETERS
+else
+  python $RUN_PY_SCRIPT \
+  $PARAMETERS
+fi
 
 
 
