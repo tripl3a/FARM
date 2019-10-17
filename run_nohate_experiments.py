2a6df03e0d84c8efdceacded1c5b2c567a654bae
tripl3a <triple_a@mailbox.org>
2019-10-17 17:01:16 +0200

    germeval-merged new grid search cfg

--- run_nohate_experiments.py
+++ run_nohate_experiments.py
@@ -8,16 +8,16 @@ def main():
         #"experiments/text_classification/germEval18Coarse_config_grid.json",
         #"experiments/text_classification/germEval18Coarse_config_best.json",
         #"experiments/text_classification/noHateCoarse_config10_grid.json",
-        #"experiments/text_classification/germEval18Coarse_config_merged_grid.json",
+        "experiments/text_classification/germEval18Coarse_config_merged_grid.json",
         #"experiments/text_classification/germEval18Coarse_config_merged_best.json",
         #"experiments/text_classification/noHateCoarse_config11_grid.json",
         #"experiments/text_classification/noHateCoarse_config12_grid.json",
         #"experiments/text_classification/noHateCoarse_config13.json",
-        "experiments/text_classification/noHateCoarse_config14_lm1.json",
-        "experiments/text_classification/noHateCoarse_config14_lm2.json",
-        "experiments/text_classification/noHateCoarse_config14_lm3.json",
-        "experiments/text_classification/noHateCoarse_config14_lm4.json",
-        "experiments/text_classification/noHateCoarse_config14_lm5.json",
+        #"experiments/text_classification/noHateCoarse_config14_lm1.json",
+        #"experiments/text_classification/noHateCoarse_config14_lm2.json",
+        #"experiments/text_classification/noHateCoarse_config14_lm3.json",
+        #"experiments/text_classification/noHateCoarse_config14_lm4.json",
+        #"experiments/text_classification/noHateCoarse_config14_lm5.json",
     ]
 
     for conf_file in config_files:
