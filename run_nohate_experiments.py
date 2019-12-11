bdbaacc50fbf501fad602bf2167e7976130cbd4a
tripl3a <triple_a@mailbox.org>
2019-12-11 13:40:18 +0100

    cv configs

--- run_nohate_experiments.py
+++ run_nohate_experiments.py
@@ -1,8 +1,14 @@
+import argparse
+import datetime
 from farm.experiment import run_experiment, load_experiments
 
 
+def get_current_datetime():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
 def main():
-    config_files = [
+    #config_files = [
         #"experiments/text_classification/noHateCoarse_config08.json",
         #"experiments/text_classification/noHateCoarse_config09.json",
         #"experiments/text_classification/germEval18Coarse_config_grid.json",
@@ -18,14 +24,24 @@ def main():
         #"experiments/text_classification/noHateCoarse_config14_lm3.json",
         #experiments/text_classification/noHateCoarse_config14_lm4.json",
         #experiments/text_classification/noHateCoarse_config14_lm5.json",
-         "experiments/text_classification/noHateCoarse_local_mlflow.json"
-    ]
+    #]
 
-    for conf_file in config_files:
+    for conf_file in args.config_files:
         experiments = load_experiments(conf_file)
         for experiment in experiments:
             run_experiment(experiment)
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-cf', '--config_files', action='append', help='<Required> Config files to run', required=True)
+
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args))
+    print("Start datetime:", get_current_datetime())
     main()
+    print("End datetime:", get_current_datetime())
+
